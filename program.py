163ce559d679fea1cca5849aefe845ba0e61c6a0
Satvik00777 <141251352+Satvik00777@users.noreply.github.com>
2024-04-14 20:19:53 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,6 @@
+#sum of two numbers using user input
+
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+sum_of_two_numbers=num1+num2
+print("Sum of two numbers :23",sum_of_two_numbers)
